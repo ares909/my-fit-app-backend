84ad3fdb6d3f996c85c27ccb1d00cab6387a7ef9
Dmitriy Khomyakov <ares909@yandex.ru>
2023-12-12 16:46:54 +0300

    feat(dev-1): users-module-added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserInDto } from './dto/create-user-in-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: CreateUserInDto) {
+    return this.usersService.createUser(userDto);
+  }
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.entity';
+import { CreateUserInDto } from './dto/create-user-in-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async createUser(dto: CreateUserInDto) {
+    return this.usersRepository.save(dto);
+  }
+  async getAllUsers() {
+    return this.usersRepository.find();
+  }
+}
